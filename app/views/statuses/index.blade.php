f055d4d0f5b36f8fc3a82e9cb0b0d8ea3bcdf877
jonathan robinson <jonbrobinson@gmail.com>
2015-04-26 23:49:14 -0500

    Lesson 19: added some css to status page

--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -2,25 +2,35 @@
 
 @section('content')
 
-    <h1>Post a Status</h1>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
 
-    @include('layouts.partials.errors')
+            @include('layouts.partials.errors')
 
-    {{ Form::open() }}
-        <div class="form-group">
-            {{ Form::label('body', 'Status:') }}
-            {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+            {{ Form::open() }}
+            <div class="form-group">
+                {{ Form::label('body', 'Status:') }}
+                {{ Form::textarea('body', null, ['class' => 'form-control', "placeholder" => "Whats on your mind?"]) }}
+            </div>
+
+            <div class="form-group">
+                {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
+            </div>
+            {{ Form::close() }}
+
+            @foreach($statuses as $status)
+                <article class="media status-media">
+                    <div class="pull-left">
+                        <img class="media-object" src="{{ $status->user->present()->gravatar }}" alt="{{ $status->user->username }}">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">{{ $status->user->username }}</h4>
+                        <p>{{ $status->created_at->diffForHumans() }}</p>
+
+                        {{ $status->body }}
+                    </div>
+                </article>
+            @endforeach
         </div>
-
-        <div class="form-group">
-            {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
-        </div>
-    {{ Form::close() }}
-
-    <h1>Statuses</h1>
-    @foreach($statuses as $status)
-        <article>
-            {{ $status->body }}
-        </article>
-    @endforeach
+    </div>
 @stop
