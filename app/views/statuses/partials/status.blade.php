f9ad6a4bd20244ee62fe83cac860d93c9ff49f8c
jonathan robinson <jonbrobinson@gmail.com>
2015-05-02 00:54:24 -0500

    cleaned code readability

--- app/views/statuses/partials/status.blade.php
+++ app/views/statuses/partials/status.blade.php
@@ -1,11 +1,11 @@
 <article class="media status-media">
-                    <div class="pull-left">
-                        @include('layouts.partials.avatar', [$user = $status->user])
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">{{ $status->user->username }}</h4>
-<p>{{ $status->present()->timeSincePublished }}</p>
+    <div class="pull-left">
+        @include('layouts.partials.avatar', [$user = $status->user])
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading">{{ $status->user->username }}</h4>
+        <p>{{ $status->present()->timeSincePublished }}</p>
 
-{{ $status->body }}
-</div>
+        {{ $status->body }}
+    </div>
 </article>
