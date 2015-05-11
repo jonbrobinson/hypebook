374c7c533029aeafb48b406350cb161941a06f6c
jonathan robinson <jonbrobinson@gmail.com>
2015-05-10 21:42:08 -0500

    display statuses of followed users

--- app/views/users/partials/follow-form.blade.php
+++ app/views/users/partials/follow-form.blade.php
@@ -1,3 +1,8 @@
-{{ Form::open() }}
-    <button type="submit" class="btn btn-primary"> Follow {{ $user->username }}</button>
-{{ Form::close() }}
+@if ($user->isFollowedBy($currentUser))
+    <p>You are following {{ $user->username }}.</p>
+@else
+    {{ Form::open(['route' => 'follows_path']) }}
+        {{ Form::hidden('userIdToFollow', $user->id) }}
+        <button type="submit" class="btn btn-primary"> Follow {{ $user->username }}</button>
+    {{ Form::close() }}
+@endif
