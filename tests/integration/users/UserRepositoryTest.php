0dd67d239677c0323a64c7a0a497ba6fe2d01d5f
jonathan robinson <jonbrobinson@gmail.com>
2015-05-17 21:18:05 -0500

    added integration test for following users

--- tests/integration/users/UserRepositoryTest.php
+++ tests/integration/users/UserRepositoryTest.php
@@ -42,5 +42,45 @@ class UserRepositoryTest extends \Codeception\TestCase\Test
         $this->assertCount(3, $user->statuses);
     }
 
+    /** @test */
+    public function it_follows_another_user()
+    {
+        //given I have two users
+        $users = TestDummy::times(2)->create('Hypebook\Users\User');
+
+        //and one user follows another user
+        $this->repo->follow($users[1]->id, $users[0]);
+
+        //then
+        $this->assertCount(1, $users[0]->followedUsers);
+
+        $this->tester->seeRecord('follows', [
+            'follower_id' => $users[0]->id,
+            'followed_id' => $users[1]->id
+        ]);
+
+    }
+
+    /** @test */
+    public function it_unfollows_another_user()
+    {
+        //given I have two users
+        $users = TestDummy::times(2)->create('Hypebook\Users\User');
+
+        //and one user follows another user
+        $this->repo->follow($users[1]->id, $users[0]);
+
+        // when I unfollow that same user
+        $this->repo->unfollow($users[1]->id, $users[0]);
+
+        //then
+        $this->assertCount(0, $users[0]->followedUsers);
+
+        $this->tester->dontSeeRecord('follows', [
+            'follower_id' => $users[0]->id,
+            'followed_id' => $users[1]->id
+        ]);
+
+    }
 
 }
